4205830b9b68366170e473159fd31abf96567747
m0kova01 <m0kova01@louisville.edu>
2019-11-21 23:56:28 -0500

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PlayerSelectionComponent } from './player-selection/player-selection.component';
+import { AppComponent } from './app.component';
+import { HighscoresComponent } from './highscores/highscores.component';
+import { HomeComponent} from './home/home.component'
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'highscores', component: HighscoresComponent},
+  {path: 'player-selection', component: PlayerSelectionComponent},
+  {path: 'home', component: HomeComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
+import { NgModule } from '@angular/core';  
+import { AppRoutingModule} from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FlexLayoutModule } from "@angular/flex-layout";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+import { HighscoresComponent } from './highscores/highscores.component';
+import { PlayerSelectionComponent } from './player-selection/player-selection.component';
+import { HomeComponent } from './home/home.component';
+import { RouterModule } from '@angular/router';
+import {MatListModule} from '@angular/material/list';
+import {MatDialogModule, MatDialogTitle} from '@angular/material/dialog';
+import {MatInputModule, MatInput} from '@angular/material/input';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HighscoresComponent,
+    PlayerSelectionComponent,
+    HomeComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FlexLayoutModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    RouterModule,
+    MatListModule,
+    MatDialogModule,
+    MatInputModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
+
