e418d69aa552448731aa77676a4b2ca7aa191d70
m0kova01 <m0kova01@louisville.edu>
2019-12-05 15:14:15 -0500

    fixed issue where players could start game without entering a name

--- src/app/player-selection/player-selection.component.ts
+++ src/app/player-selection/player-selection.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from "@angular/core";
 import { PlayerObject } from "../models/PlayerObject";
 import { DataService } from "../services/data.service";
 import { MatSnackBar } from "@angular/material/snack-bar";
-import { UtilityService } from '../services/utility.service';
+import { UtilityService } from "../services/utility.service";
 
 @Component({
   selector: "app-player-selection",
@@ -10,11 +10,27 @@ import { UtilityService } from '../services/utility.service';
   styleUrls: ["./player-selection.component.css"]
 })
 export class PlayerSelectionComponent implements OnInit {
-  constructor(private data: DataService, private _snackBar: MatSnackBar, private utility: UtilityService) { }
+  enoughPlayers: boolean;
+  constructor(
+    private data: DataService,
+    private _snackBar: MatSnackBar,
+    private utility: UtilityService
+  ) {
+    if(this.data.playerCount() < 1)
+    {
+      this.enoughPlayers = false;
+    }
+    else
+    {
+      this.enoughPlayers = true;
+    }
+  }
+
   isMobile: boolean;
+
   ngOnInit() {
     this.isMobile = this.utility.GetMediaQuery();
-   }
+  }
 
   pushPlayer() {
     var element = <HTMLInputElement>document.getElementById("nameInput");
@@ -22,11 +38,20 @@ export class PlayerSelectionComponent implements OnInit {
       .value;
     var playerObject = new PlayerObject(nameInput);
     this.data.addPlayer(playerObject);
+    this.enoughPlayers = true;
 
     element.value = "";
   }
+
+  noPlayersMessage() {
+    this._snackBar.open("Need at least one person to play!", "Hide", {
+      duration: 2000
+    });
+  }
+
   openSnackBar() {
-    var nameInput = (<HTMLInputElement>document.getElementById("nameInput")).value;
+    var nameInput = (<HTMLInputElement>document.getElementById("nameInput"))
+      .value;
     this._snackBar.open(nameInput + " added!", "Hide", {
       duration: 2000
     });
