f5142f56f0b6003fecb20c7cd0e81603e6b63e5f
Mate Kovacs <m0kova01@louisville.edu>
2019-12-22 10:01:20 -0500

    added list of player names in player-selection.component. this allows users to see and remove players from the game

--- src/app/player-selection/player-selection.component.ts
+++ src/app/player-selection/player-selection.component.ts
@@ -20,6 +20,7 @@ export class PlayerSelectionComponent implements OnInit {
   isMobile: boolean;
 
   ngOnInit() {
+    this.data.clearPlayerArr();
     if (this.data.playerCount() < 1) {
       this.enoughPlayers = false;
     } else {
@@ -38,6 +39,16 @@ export class PlayerSelectionComponent implements OnInit {
       this.playerAddedSnackBar(nameInput);
     }
   }
+  removePlayer(playerToDelete: PlayerObject) {
+    const index = this.data.playerArray.indexOf(playerToDelete, 0);
+    if (index > -1) {
+      this.data.playerArray.splice(index, 1);
+    }
+    this.playerRemovedSnackBar(playerToDelete.playerName);
+    if (this.data.playerArray.length === 0) {
+      this.enoughPlayers = false;
+    }
+  }
 
   playerAddedSnackBar(name: string) {
     this._snackBar.open(name + " added!", "Hide", {
@@ -47,6 +58,11 @@ export class PlayerSelectionComponent implements OnInit {
 
   noPlayerEnteredSnackBar() {
     this._snackBar.open("Please enter a player name!", "Hide", {
+      duration: 2000
+    });
+  }
+  playerRemovedSnackBar(name: string) {
+    this._snackBar.open(name + " removed!", "Hide", {
       duration: 2000
     });
   }
